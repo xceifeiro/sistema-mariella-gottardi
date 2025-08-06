9a2da69ba31c847895db51266bbff7116852315c
xceifeiro <gomesvictor46@gmail.com>
2025-08-06 10:55:05 -0300

    bug fixes

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,7 @@
-// lib/db.ts
 import postgres from 'postgres'
 
 const sql = postgres(process.env.DATABASE_URL!, {
-  ssl: process.env.NODE_ENV === 'production',
+  ssl: process.env.DATABASE_USE_SSL === 'true',
 })
 
 export default sql
